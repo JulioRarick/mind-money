972d176ac9622c7632135b1b77714535b8b9a8dd
Julio Rarick <juliorarick@gmail.com>
2024-06-22 04:09:04 -0300

    ⚡️ perf: add use-context-selector

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -11,8 +11,8 @@ import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react';
 import * as z from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Controller, useForm } from 'react-hook-form';
-import { useContext } from 'react';
 import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { useContextSelector } from 'use-context-selector';
 
 const newTransactionFormSchema = z.object({
    description: z.string(),
@@ -23,7 +23,12 @@ const newTransactionFormSchema = z.object({
 type NewTransactionFormInputs = z.infer<typeof newTransactionFormSchema>;
 
 export function NewTransactionModal() {
-   const { createTransactions } = useContext(TransactionsContext);
+   const createTransactions = useContextSelector(
+      TransactionsContext,
+      (context) => {
+         return context.createTransactions;
+      }
+   );
 
    const {
       control,
