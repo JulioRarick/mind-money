b4226c6153d131e55fb8ffe148c98c68a3235cf6
Julio Rarick <juliorarick@gmail.com>
2024-06-21 21:45:19 -0300

    ✨ feat: create search form component

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 export const HeaderContainer = styled.header`
    background: ${(props) => props.theme['gray-300']};
    padding: 2.5rem 0 7.5rem;
+   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
 `;
 export const HeaderContent = styled.div`
    width: 100%;
@@ -30,6 +31,7 @@ export const NewTransactionsButton = styled.button`
    font-weight: bold;
    padding: 0 1.25rem;
    border-radius: 6px;
+   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
    cursor: pointer;
    transition: 0.3s ease-in-out;
    &:hover {
---
+++ src/pages/Transactions/components/SearchForm/styles.ts
@@ -0,0 +1,39 @@
+import styled from 'styled-components';
+
+export const SearchFormContainer = styled.form`
+   display: flex;
+   gap: 1rem;
+
+   input {
+      flex: 1;
+      border-radius: 6px;
+      border: 0;
+      background: ${(props) => props.theme['gray-300']};
+      padding: 1rem;
+
+      &::placeholder {
+         color: ${(props) => props.theme['gray-500']};
+      }
+   }
+   button {
+      display: flex;
+      align-items: center;
+      gap: 0.75rem;
+
+      border: 0;
+      background: transparent;
+      padding: 1rem;
+      border: 1px solid ${(props) => props.theme['green-400']};
+      color: ${(props) => props.theme['green-400']};
+      font-weight: bold;
+      border-radius: 6px;
+      cursor: pointer;
+      transition: 0.3s ease-in-out;
+
+      &:hover {
+         background: ${(props) => props.theme['green-500']};
+         border-color: 1px solid ${(props) => props.theme['green-500']};
+         color: ${(props) => props.theme.white};
+      }
+   }
+`;
